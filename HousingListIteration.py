87a3e6f985ceed049f5cec2a29d2750661db02bb
Ouchoh <briannouchoh@gmail.com>
2022-06-27 18:00:27 +0300

    Tabulation and iteration

--- HousingListIteration.py
+++ HousingListIteration.py
@@ -19,3 +19,41 @@ house.append(price_m2)
 
 houses_nested_list
 
+#Creating a dictionary
+
+house_0_dict = {
+    "price_aprox_usd": 115910.26,
+    "surface_covered_in_m2": 128,
+    "rooms": 4,
+}
+
+#Calculating the price per sqaure meter for house_0_dict and add it to the dictionary under the key "price_per_m2"
+
+#using JSON to store data as a list of dictionaries
+houses_rowwise = [
+    {
+        "price_aprox_usd": 115910.26,
+        "surface_covered_in_m2": 128,
+        "rooms": 4,
+    },
+    {
+        "price_aprox_usd": 48718.17,
+        "surface_covered_in_m2": 210,
+        "rooms": 3,
+    },
+    {
+        "price_aprox_usd": 28977.56,
+        "surface_covered_in_m2": 58,
+        "rooms": 2,
+    },
+    {
+        "price_aprox_usd": 36932.27,
+        "surface_covered_in_m2": 79,
+        "rooms": 3,
+    },
+    {
+        "price_aprox_usd": 83903.51,
+        "surface_covered_in_m2": 111,
+        "rooms": 3,
+    },
+]
